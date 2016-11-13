78abd29cf8e341adfd41ce379c03e3a2b4318326
bortne <kristsbo@stud.ntnu.no>
2016-11-13 16:37:48 +0100

    - Added some documentation

--- Exercise2/src/DatapathLane.vhd
+++ Exercise2/src/DatapathLane.vhd
@@ -23,22 +23,23 @@ architecture Behavioural of DatapathLane is
   signal decoder_in  : decoder_in_t;
   signal decoder_out : decoder_out_t;
 begin
-
+--Sends the instruction to decode
   decoder_in.instruction <= input.instruction;
-  Decoder_1: entity work.Decoder
-    port map (
-      input  => decoder_in,
-      output => decoder_out);
+--Signals going to the registerfile
   output.reg1_addr <= decoder_out.reg1_addr;
   output.reg2_addr <= decoder_out.reg2_addr;
   output.reg_dst <= decoder_out.reg_dest;
   output.reg_we <= decoder_out.reg_we;
-
+--Gives input to the ALU
   alu_op_a <= input.reg1_value;
   alu_op_b <= input.reg2_value;
   alu_operation <= ALU_OP_ADD;
   shift_amount <= (others => '0');
-  Alu_1: entity work.Alu
+--Recieve the result from ALU
+  output.reg_value <= result;
+
+--Initialize modules
+Alu_1: entity work.Alu
     port map (
       alu_op_a      => alu_op_a,
       alu_op_b      => alu_op_b,
@@ -46,6 +47,8 @@ begin
       alu_operation => alu_operation,
       result        => result,
       is_res_zero   => is_res_zero);
-  output.reg_value <= result;
-
+Decoder_1: entity work.Decoder
+    port map (
+      input  => decoder_in,
+      output => decoder_out);
 end architecture Behavioural;
--- Exercise2/src/MIPSProcessor.vhd
+++ Exercise2/src/MIPSProcessor.vhd
@@ -16,20 +16,21 @@ entity MIPSProcessor is
 end MIPSProcessor;
 
 architecture DualIssue of MIPSProcessor is
+--Signals for fetch unit
   signal if_in  : instruction_fetch_in_t;
   signal if_out : instruction_fetch_out_t;
 
-  -- Register file signals
+-- Register file signals
   signal rf_in  : regfile_in_t;
   signal rf_out : regfile_out_t;
-
+-- Datapath signal for path1 and path2
   signal dpath1_in, dpath2_in  : datapath_lane_in_t;
   signal dpath1_out, dpath2_out : datapath_lane_out_t;
-
+--Activation signal for activation of the two lanes
   signal lane1_active, lane2_active : boolean;
 
 begin
-
+--A check to start the processor
   process (input.clk) is
   begin
     if rising_edge(input.clk) then
@@ -37,39 +38,26 @@ begin
       lane2_active <= input.processor_enable = '1';
     end if;
   end process;
-
+--Turn on fetch unit when processor is on
   if_in.processor_enable <= input.processor_enable;
-  InstructionFetch_1: entity work.InstructionFetch
-    port map (
-      input  => if_in,
-      output => if_out);
+--Program counter address goes to IMEM
   output.imem_address <= if_out.address;
 
+--Giving ds and dt to registerfile
   rf_in.read_ports(0) <= dpath1_out.reg1_addr;
   rf_in.read_ports(1) <= dpath1_out.reg2_addr;
   rf_in.read_ports(2) <= dpath2_out.reg1_addr;
   rf_in.read_ports(3) <= dpath2_out.reg2_addr;
-  RegisterFile_1 : RegisterFile
-    port map (
-      input  => rf_in,
-      output => rf_out,
-      debug_input => rf_debug_in,
-      debug_output => rf_debug_out);
-
+  
+--Signals for input to datalanes
   dpath1_in.instruction <= input.imem_data(63 downto 32);
   dpath2_in.instruction <= input.imem_data(31 downto 0);
   dpath1_in.reg1_value <= rf_out.read_ports(0);
   dpath1_in.reg2_value <= rf_out.read_ports(1);
   dpath2_in.reg1_value <= rf_out.read_ports(2);
   dpath2_in.reg2_value <= rf_out.read_ports(3);
-  DatapathLane_1: entity work.DatapathLane
-    port map (
-      input  => dpath1_in,
-      output => dpath1_out);
-  DatapathLane_2: entity work.DatapathLane
-    port map (
-      input  => dpath2_in,
-      output => dpath2_out);
+  
+--Write signal to registerfile that comes from two different datalanes
   rf_in.write_ports(0).dst <= dpath1_out.reg_dst;
   rf_in.write_ports(0).value <= dpath1_out.reg_value;
   rf_in.write_ports(0).we <= dpath1_out.reg_we and lane1_active;
@@ -77,11 +65,35 @@ begin
   rf_in.write_ports(1).value <= dpath2_out.reg_value;
   rf_in.write_ports(1).we <= dpath2_out.reg_we and lane2_active;
 
-  -- Feed clock, reset and processor_enable to all components which need it.
+-- Feed clock, reset and processor_enable to all components which need it.
   rf_in.clk <= input.clk;
   rf_in.rst <= input.rst;
 
   if_in.clk <= input.clk;
   if_in.rst <= input.rst;
+
+--Initialize the different modules
+--First datapath lane
+DatapathLane_1: entity work.DatapathLane
+    port map (
+      input  => dpath1_in,
+      output => dpath1_out);
+--Second datapath lane
+  DatapathLane_2: entity work.DatapathLane
+    port map (
+      input  => dpath2_in,
+      output => dpath2_out);
+--Program counter or fetch unit
+InstructionFetch_1: entity work.InstructionFetch
+    port map (
+      input  => if_in,
+      output => if_out);
+--Register file
+RegisterFile_1 : RegisterFile
+    port map (
+      input  => rf_in,
+      output => rf_out,
+      debug_input => rf_debug_in,
+      debug_output => rf_debug_out);
 
 end DualIssue;
